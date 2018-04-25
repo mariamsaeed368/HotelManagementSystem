8cd9a446f47a6f8ca42d88bb896deb9427611956
mariamsaeed368 <mariamsaeed84@gmail.com>
2018-04-25 19:04:49 +0500

    we all did this.(khadija,asma,farva,mariam).

---
+++ WindowsFormsApplication9/WindowsFormsApplication9/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace WindowsFormsApplication9
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            localhost.Service1 server = new localhost.Service1();
+            BindingSource source = new BindingSource();
+            source.DataSource = server.show_all();
+            dataGridView1.DataSource = source;
+        }
+
+       
+
+        private void linkLabel1_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form1 p = new Form1();
+            p.Show();
+            this.Hide();
+
+        }
+    }
+}
---
+++ myserver/myserver/guestdl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace myserver
+{
+    public class guestdl
+    {
+        public static List<guest> p = new List<guest>();
+    }
+}
