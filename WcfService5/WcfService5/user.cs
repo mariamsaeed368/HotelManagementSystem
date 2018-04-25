292565200b399db9c84601b099ea5240eb6d6722
mariamsaeed368 <mariamsaeed84@gmail.com>
2018-04-25 15:56:54 +0500

    I have implemented the server.

---
+++ WcfService5/WcfService5/user.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace WcfService5
+{
+    [DataContract]
+    public class user
+    {
+        private string users;
+        [DataMember]
+        public string User
+        {
+            get { return users; }
+            set { users = value; }
+        }
+        private string password;
+        [DataMember]
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
