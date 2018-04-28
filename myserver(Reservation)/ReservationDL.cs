b8e741680376687e283b557f7b0d14229bc8e555
khadija ghafoor <khadijaghafoor00@gmail.com>
2018-04-28 18:35:29 +0500

    i have made Reservation.cs and ReservationDL.cs in myserver.

---
+++ myserver(Reservation)/ReservationDL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Linq;
+using System.Web;
+
+namespace myserver
+{
+    public class ReservationDL
+    {
+        public static List<Reservation> resevations = new List<Reservation>();
+        public void roomReservation()
+        {
+            Reservation k=new Reservation();
+            if(k.Isfree)
+            {
+                k.Status="Free";
+                ReservationDL.resevations.Add(k);
+            }
+            else
+            {
+                k.Status="Busy";
+            }
+        }
+    }
+}
