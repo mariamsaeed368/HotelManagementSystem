488e2e695479558adca5a932c17c3d6c67c0c59a
mariamsaeed368 <mariamsaeed84@gmail.com>
2018-05-05 10:56:39 +0500

    for the sake of testing i did implement the employee management which will add the employee in the system and show the details of the employee.

---
+++ employee_management/employee_management/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace employee_management
+{
+    public partial class Form1 : Form
+    {
+    
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                employee t = new employee();
+                t.Name = textBox1.Text;
+                t.Id1 = int.Parse(textBox2.Text);
+                t.Phone_no = textBox3.Text;
+                t.Department1 = textBox4.Text;
+                 employeedl.employee.Add(t);
+                MessageBox.Show("Employee has been added.");
+                textBox1.Text = " ";
+                textBox2.Text = " ";
+                textBox3.Text = " ";
+                textBox4.Text = " ";
+       }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            BindingSource b = new BindingSource();
+            b.DataSource = employeedl.employee;
+            dataGridView1.DataSource = b;
+
+        }
+    }
+    
+}
---
+++ employee_management/employee_management/employee.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee_management
+{
+    class employee
+    {
+        private int Id;
+        private string name;
+        private string phone_no;
+        private string Department;
+        public int Id1
+        {
+            get
+            {
+                return Id;
+            }
+
+            set
+            {
+                Id = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Phone_no
+        {
+            get
+            {
+                return phone_no;
+            }
+
+            set
+            {
+                phone_no = value;
+            }
+        }
+
+        public string Department1
+        {
+            get
+            {
+                return Department;
+            }
+
+            set
+            {
+                Department = value;
+            }
+        }
+
+       
+
+    }
+}
